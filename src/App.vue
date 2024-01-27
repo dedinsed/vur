90591263c37661b4b2db2a28d56abf4f8380a167
dedinsed <1000m7zxc@gmail.com>
2024-01-27 17:00:24 +0300

    lesson2

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,16 @@
 <template>
   <div class="container">
-    <div class="row">  
-      <test2 msg="farefox" foto="https://avatars.mds.yandex.net/i?id=19fe06ba659bdb27cfa71dc5c6ec54aaced21b5e-12159448-images-thumbs&n=13"/>
-      <test2 msg="farefox" foto="https://gas-kvas.com/grafic/uploads/posts/2023-10/1696581493_gas-kvas-com-p-kartinki-vsyakie-9.jpg/"
-      <test2 msg="nextgen" foto="https://avatars.mds.yandex.net/i?id=26eef3948c699b5b1000dc9e1bb2f068db1ddf4d-10995124-images-thumbs&n=13/"
-      
-     </div>   
-   </div>
+   <h1>Учёт корзин</h1
+    </div>
 </template>
 
 <script>
-import test2 from './components/test2.vue' 
+import card from './components/CARD.vue' 
 
 export default {
   name: 'App',
   components: {
-    test2
+   
   }
 }
 </script>
---
+++ src/components/card.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="rows">
+<form class="row g-3">
+  <div class="col-auto">
+    <label for="staticEmail2" class="visually-hidden">Название</label>
+    <input type="text" readonly class="form-control-plaintext" id="staticEmail2" value="email@example.com">
+  </div>
+  <div class="col-auto">
+    <label for="inputPassword2" class="visually-hidden">Цена за шт</label>
+    <input type="password" class="form-control" id="inputPassword2" placeholder="Цена за шт">
+  </div>
+   <div class="col-auto">
+    <label for="inputPassword2" class="visually-hidden">Кол-во</label>
+    <input type="password" class="form-control" id="inputPassword2" placeholder="Кол-во">
+  </div>
+  <div class="col-auto">
+    <button type="submit" class="btn btn-primary mb-3">Confirm identity</button>
+  </div>
+</form>
+
+  </div>
+</template>
+ 
+<script>
+/* eslint-disable */
+export default {
+  name: 'card',
+  props: {
+    msg: String
+    
+  }
+}
+</script>
+
+<
+</style>
