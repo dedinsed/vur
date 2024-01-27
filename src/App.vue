76c8203a26bdfef2833702412cb2f212fcd4cb3f
dedinsed <1000m7zxc@gmail.com>
2024-01-27 16:02:17 +0300

    lesson2

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">  
+      <test2 msg="farefox" foto="https://avatars.mds.yandex.net/i?id=19fe06ba659bdb27cfa71dc5c6ec54aaced21b5e-12159448-images-thumbs&n=13"/>
+      <test2 msg="farefox" foto="https://gas-kvas.com/grafic/uploads/posts/2023-10/1696581493_gas-kvas-com-p-kartinki-vsyakie-9.jpg/"
+      <test2 msg="nextgen" foto="https://avatars.mds.yandex.net/i?id=26eef3948c699b5b1000dc9e1bb2f068db1ddf4d-10995124-images-thumbs&n=13/"
+      
+     </div>   
+   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import test2 from './components/test2.vue' 
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    test2
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/test2.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="hello">
+ <div class="card" style="width: 18rem;">
+  <img v-if="check" v-bind:src="foto" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">{{check}}</h5>
+    <p class="card-text">{{msg}}</p>
+    <button v-on:click="show" type="button" class="btn btn-primary">{{msgBtn}}</button>
+  </div>
+</div>
+  </div>
+</template>
+ 
+<script>
+/* eslint-disable */
+export default {
+  name: 'test2',
+  props: {
+    msg: String,
+    foto: String
+  },
+  methods:{
+    show(){
+      this.check = ! this.check
+      if(this.check == true){
+        this.msgBtn = "Скрыть"
+      }else{
+        this.msgBtn = "Показать"
+      }
+    }
+  },
+  data(){
+    return{
+      check: true,
+      msgBtn: "Скрыть"
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
